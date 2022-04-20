81ae43f3b6a62544774be2d7ac2e8a01209e1de3
Gabriel <gabrielolobo42@gmail.com>
2022-04-20 18:18:55 -0300

    creates tool

---
+++ AutofillDocs.js
@@ -0,0 +1,13 @@
+function onOpen() {
+    const ui = SpreadsheetApp.getUi();
+    const menu = ui.createMenu('AutoFill Docs');
+    menu.addItem ('Create Term','createNewGoogleDocs');
+    menu.addToUi();
+    
+    }
+    
+    function createNewGoogleDocs() {
+    
+    
+      
+    }
