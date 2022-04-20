33464137a633fbc0f460678fa64d0a919a807d3f
Gabriel <gabrielolobo42@gmail.com>
2022-04-20 19:00:41 -0300

    Produce terms

--- AutofillDocs.js
+++ AutofillDocs.js
@@ -4,12 +4,22 @@ function onOpen() {
     menu.addItem ('Create Term','createNewGoogleDocs');
     menu.addToUi();
     
-    }
+ }
     
-    function createNewGoogleDocs() {
+function createNewGoogleDocs() {
     
     const googleDocTemplate = DriveApp.getFileById('Document ID : Stored in the URL');
     const destinationFolder = DriveApp.getFolderById('Folder ID: Stored in the URL');
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Name of the Sheet');
+    const rows = sheet.getDataRange().getValues();
 
-    }
+    rows.forEach(function(row, index){
+        if (index === 0) return;
+        if (row['Term Link Number']) return;
+
+    
+    
+    
+    })
+ 
+ }
